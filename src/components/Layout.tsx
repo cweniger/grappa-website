336b6f98677472017207bcfffefa81621807174a
tatianamac <github@tatianamac.com>
2021-01-11 17:47:36 -0800

    Connects sass

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,33 +18,11 @@ export default function Layout({ children }: Props) {
         <link rel="stylesheet" href="/fonts/style.css" />
          
       </Head>
+
       <nav className={styles.navigation}>
         <Navigation />
       </nav>
       <main>{children}</main>
-      <style jsx>
-        {`
-          .root {
-            display: block;
-            padding: 4rem 0;
-            box-sizing: border-box;
-            height: 100%;
-          }
-          main {
-            display: flex;
-            min-height: 100%;
-          }
-          @media (min-width: 769px) {
-            .root {
-              display: flex;
-              flex: 1 0 auto;
-            }
-            main {
-              flex: 1 0 auto;
-            }
-          }
-        `}
-      </style>
     </div>
   );
 }
