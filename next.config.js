828cace2d0e43f66ced33ac338c81728c72a9562
Tatiana Mac <github@tatianamac.com>
2021-01-13 13:17:43 -0800

    Fixes dist setting

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 // const rehypePrism = require("@mapbox/rehype-prism");
 
 module.exports = {
-  distDir: 'dist',
+  distDir: '../dist',
   webpack: (config) => {
     config.module.rules.push(
       ...[
