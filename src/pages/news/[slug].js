a1a627a0700c09d0b0fddf14afe10fa3be8cf1b0
Tatiana Mac <github@tatianamac.com>
2021-12-01 18:11:46 -0800

    Try it????

--- src/pages/news/[slug].js
+++ src/pages/news/[slug].js
@@ -2,7 +2,6 @@ import Layout from "../../components/Layout";
 import BasicMeta from "../../components/meta/BasicMeta";
 import ReactMarkdown from "react-markdown";
 import React from "react";
-import classNames from "classnames";
 
 export default function News({ article }) {
   const formattedDate = new Date(article.date).toLocaleDateString("en-GB", {
@@ -42,54 +41,13 @@ export default function News({ article }) {
   );
 }
 
-const spaceId = process.env.CONTENTFUL_SPACE;
-const environmentId = process.env.CONTENTFUL_ENV;
-const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
-const contentType = "news";
-
 export async function getStaticPaths() {
-  // Call an external API endpoint to get member posts
-  const URL = `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${accessToken}&content_type=${contentType}`;
-  const res = await fetch(URL);
-  const articles = await res.json();
-
-  // Get the paths we want to pre-render based on member posts
-  const articlesWithSlug = articles.items.filter(
-    (article) => article.fields.slug !== undefined
-  );
-  const paths = articlesWithSlug.map(
-    (article) => `/news/${article.fields.slug}`
-  );
-
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
-  return { paths, fallback: false };
-}
-
-export async function getStaticProps({ params }) {
-  const URL = `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${accessToken}&fields.slug=${params.slug}&content_type=${contentType}`;
-
-  const response = await fetch(URL);
-  const articles = await response.json();
-  const article = articles.items[0].fields;
-
-  if (!article.image) {
-    return {
-      props: {
-        article: {
-          ...article,
-        },
-      },
-    };
-  }
-
-  const asset = articles.includes.Asset[0];
+  const posts = await getAllNewsSlugs();
+  const paths = posts?.newsCollection?.items.map((page) => {
+    return { params: { slug: page.slug } };
+  });
   return {
-    props: {
-      article: {
-        ...article,
-        image: `https:${asset.fields.file.url}`,
-      },
-    },
+    paths,
+    fallback: "blocking",
   };
 }
