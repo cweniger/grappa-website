c72364c41fdeffd52652bad3e46c0581888fe86e
tatianamac <github@tatianamac.com>
2022-04-08 10:23:23 -0700

    fix: Changes research into its own page type in contentful

--- src/pages/research/index.tsx
+++ src/pages/research/index.tsx
@@ -2,54 +2,47 @@ import { gql } from "graphql-request";
 import Layout from "../../components/Layout";
 import SecondaryHero from "../../components/SecondaryHero";
 import BasicMeta from "../../components/meta/BasicMeta";
-import layout from "../../styles/components/Layout.module.scss";
 import { contentfulApi } from "../../lib/contentful";
 import classnames from "classnames";
 import Card from "../../components/Card";
-import Row from "../../components/Row";
 import React from "react";
-import groupBy from "lodash.groupby";
 
 import research from "../../styles/components/Research.module.scss";
-export default function Research({ entry, heroEntry }) {
-  const researchByType = groupBy(entry.researchCollection.items, (area) => {
-    if (area.researchType == "Research Theme") {
-      return "theme";
-    } else {
-      return "experiment";
-    }
-  });
+export default function Research({ pageEntry }) {
   return (
     <Layout>
-      <BasicMeta url={"/"} />
-      <SecondaryHero heroEntry={heroEntry.hero} />
-
-      <section
-        className={classnames(
-          research.container,
-          "container__main container__flex container__flex--space-between"
-        )}
-      >
-        {researchByType.theme.map((area) => (
-          <Card
-            key={area.title}
-            title={area.title}
-            image={area.image}
-            slug={`/research/${area.slug}`}
-          />
-        ))}
-      </section>
-
-      <section className="container__main">
-        <p className="text__headline__1">
-          We also have major areas of experimentation.
-        </p>
+      <BasicMeta
+        title={pageEntry.pageMetadata.title}
+        url={`/${pageEntry.pageMetadata.slug}`}
+      />
+      <SecondaryHero
+        className="container__research__black"
+        heroEntry={pageEntry.hero}
+        innerClassName="container__main__research"
+      />
+      <section className={research.wrapper}>
         <div
-          className={classnames("container__flex container__flex--colstatic")}
+          className={classnames(
+            research.container,
+            "container__main__research"
+          )}
         >
-          {researchByType.experiment.map((area) => (
-            <Row
+          {pageEntry.researchAreasCollection.items.map((area) => (
+            <Card
               key={area.title}
+              title={area.title}
+              image={area.image}
+              slug={`/research/${area.slug}`}
+            />
+          ))}
+        </div>
+      </section>
+      <section className="container__main">
+        <h2 className="text__headline__1">{pageEntry.experimentTitle}</h2>
+        <div className={research.container}>
+          {pageEntry.experimentAreasCollection.items.map((area) => (
+            <Card
+              key={area.sys.id}
               title={area.title}
               image={area.image}
               slug={`/research/${area.slug}`}
@@ -62,6 +55,78 @@ export default function Research({ entry, heroEntry }) {
 }
 
 export async function getStaticProps({ preview = false }) {
+  const pageQuery = gql`
+    query researchPageEntryQuery {
+      researchPage(id: "5iAwAOp78HiQV0mfCTr8dX") {
+        sys {
+          id
+        }
+        hero {
+          headline
+          subheader
+        }
+        pageMetadata {
+          title
+          slug
+          description
+        }
+        researchAreasCollection {
+          items {
+            sys {
+              id
+            }
+            title
+            abstract
+            description
+            image {
+              url
+            }
+            researchType
+            order
+            slug
+            team: teamCollection(limit: 10) {
+              items {
+                fullName
+                slug
+                omitProfile
+                profilePicture {
+                  url
+                }
+              }
+            }
+          }
+        }
+        experimentTitle
+        experimentAreasCollection {
+          items {
+            sys {
+              id
+            }
+            title
+            abstract
+            description
+            image {
+              url
+            }
+            researchType
+            order
+            slug
+            team: teamCollection(limit: 10) {
+              items {
+                fullName
+                slug
+                omitProfile
+                profilePicture {
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
   const query = gql`
     query researchCollectionQuery($preview: Boolean!) {
       researchCollection(preview: $preview, order: order_ASC) {
@@ -90,23 +155,14 @@ export async function getStaticProps({ preview = false }) {
     }
   `;
 
-  const heroQuery = gql`
-    query researchHero {
-      hero(id: "5ABoPmLRoL8Zznw6CMhkOA") {
-        headline
-        subheader
-      }
-    }
-  `;
-
+  const pageData = await contentfulApi(pageQuery, { preview });
   const data = await contentfulApi(query, { preview });
-  const heroData = await contentfulApi(heroQuery, { preview });
   const entry = data;
-  const heroEntry = heroData;
+  const pageEntry = pageData.researchPage;
   return {
     props: {
       entry,
-      heroEntry,
+      pageEntry,
       preview,
     },
   };
