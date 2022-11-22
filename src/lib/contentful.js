b0fc867bdb4efd990d0eee9529288d6a45777523
Tatiana Mac <github@tatianamac.com>
2022-11-21 18:56:17 -0800

    update

--- src/lib/contentful.js
+++ src/lib/contentful.js
@@ -1,5 +1,5 @@
-import request from "graphql-request";
-import { gql } from "@apollo/client";
+import request, { gql } from "graphql-request";
+
 const spaceId = process.env.CONTENTFUL_SPACE;
 const environmentId = process.env.CONTENTFUL_ENV;
 const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
@@ -7,7 +7,6 @@ const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
 export function contentfulApi(query, variables = {}) {
   // const accessToken = variables.preview ? previewToken : deliveryToken;
   const url = `https://graphql.contentful.com/content/v1/spaces/${spaceId}/environments/${environmentId}?access_token=${accessToken}`;
-
   return request(url, query, variables);
 }
 
