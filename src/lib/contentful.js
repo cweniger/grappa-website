d636e77b1bcdbf62f9e51926f40778c5654179b4
Tatiana Mac <github@tatianamac.com>
2022-11-30 01:30:22 -0800

    chore: lint

--- src/lib/contentful.js
+++ src/lib/contentful.js
@@ -5,24 +5,6 @@ const environmentId = process.env.CONTENTFUL_ENV;
 const deliveryToken = process.env.CONTENTFUL_ACCESS_TOKEN;
 const previewToken = process.env.CONTENTFUL_PREVIEW_TOKEN;
 
-// async function fetchGraphQL(query, preview = false) {
-//   return fetch(
-//     `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
-//     {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${
-//           preview
-//             ? process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
-//             : process.env.CONTENTFUL_ACCESS_TOKEN
-//         }`,
-//       },
-//       body: JSON.stringify({ query }),
-//     }
-//   ).then((response) => response.json());
-// }
-
 export function contentfulApi(query, variables = {}) {
   const accessToken = variables.preview ? previewToken : deliveryToken;
   const url = `https://graphql.contentful.com/content/v1/spaces/${spaceId}/environments/${environmentId}?access_token=${accessToken}`;
